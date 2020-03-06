28094afa5c0569ec65fc47f0a4e4e0783091c24b
AlexeyDemonov <alexei.demonov.341@gmail.com>
2020-03-06 11:27:33 +0500

    Code cleanup Changed: SyslogSender - removed traces of previous approach

--- SyslogSenderActionPlugin/SyslogSender.cs
+++ SyslogSenderActionPlugin/SyslogSender.cs
@@ -6,13 +6,11 @@ namespace SyslogSenderActionPlugin
     class SyslogSender : IDisposable
     {
         UdpClient _udpClient;
-        SyslogSerializer _serializer;
         bool _disposed;
 
         public SyslogSender(string host, int port)
         {
             _udpClient = new UdpClient(host, port);
-            _serializer = new SyslogSerializer();
         }
 
         public void Send(byte[] data)
@@ -23,7 +21,6 @@ namespace SyslogSenderActionPlugin
         public void Dispose()
         {
             _udpClient?.Close();
-            _serializer = null;
             _disposed = true;
         }
 
